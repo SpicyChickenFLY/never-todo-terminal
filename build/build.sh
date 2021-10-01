f4ed029be3f2df6101d08eebe920bfa3aa439766
SpicyChickenFLY <xixiayuchao@gmail.com>
2021-10-01 10:40:56 +0800

    make build script for unix system

---
+++ build/build.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+echo $(cd `dirname $0`; pwd)
+
+cd $(cd `dirname $0`; pwd)
+
+golex -o ../parser/lex.go ../parser/lex.l
+goyacc -o ../parser/parser.go ../parser/parser.y
+
+mkdir ~/.nevertodo
+cp ../static/data.json ~/.nevertodo
+
+cd ..
+
+go build -o nt
+cp nt /usr/bin
