8f3e78ea6caf61561e28a6a25f9b68e64441b19a
SpicyChickenFLY <xixiayuchao@gmail.com>
2021-10-22 15:51:24 +0800

    try to reconstruct renderer

--- render/base.go
+++ render/base.go
@@ -1,10 +1,14 @@
 package render
 
-type Table struct {
-	fieldNames     []string
-	fieldMaxLength []int
+import "fmt"
 
-	Rows []interface{}
+type Table struct {
+	pageLen       int
+	fieldNames    []string
+	fieldLenLimit []int
+	fieldMaxLen   []int
+
+	rows []Row
 }
 
 const (
@@ -13,18 +17,61 @@ const (
 )
 
 type Row struct {
-	rowType int
+	rowType     int
+	fieldValues []string
 }
 
-type Record struct {
-	fieldValue []string
+type Record []interface{}
+
+func (t *Table) calcRowMaxLen() (sum int) {
+	for _, maxLen := range t.fieldMaxLen {
+		sum += maxLen
+	}
+	return
 }
 
-func (t *Table) SetFieldName(idx int, name string) {}
-func (t *Table) SetFieldMaxLength(idx, length int) {}
+func (t *Table) SetFieldNames(fieldNames []string) {
+	if len(fieldNames) != len(t.fieldNames) {
+		// TODO: fullfill or throw exception //
+		return
+	}
+	for i, fieldName := range fieldNames {
+		t.fieldNames[i] = fieldName
+		if len(fieldName) > t.fieldMaxLen[i] {
+			t.fieldMaxLen[i] = len(fieldName)
+		}
+	}
+}
+func (t *Table) SetFieldLenLimit(idx, length int) {}
 
-func (t *Table) AppendRecord() {}
+func (t *Table) AppendRecord(record Record) {
+	if len(record) != len(t.fieldNames) {
+		// TODO: fullfill or throw exception //
+		return
+	}
+	fieldValues := make([]interface{}, 0)
+	for i, field := range record {
+		fieldContent := fmt.Sprint(field)
+		fieldValues = append(fieldValues, fieldContent)
+		if len(fieldContent) >= t.fieldMaxLen[i] {
+			t.fieldMaxLen[i] = len(fieldContent)
+		}
+	}
+	row := Row{rowTypeRecord, fieldValues}
+}
 
 func (t *Table) AppendDoubleLine() {}
 func (t *Table) AppendSolidLine()  {}
 func (t *Table) AppendEmptyLine()  {}
+
+func (t *Table) Render() {
+	if t.pageLen < t.calcRowMaxLen() {
+		// TODO:  do something  //
+	}
+	for _, row := range t.rows {
+		switch row.rowType {
+		case rowTypeRecord:
+			// TODO:  <22-10-21, yourname> //
+		}
+	}
+}
